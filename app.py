753245229689bec4ab468c15bfa4989dce5f758d
Venkatanadhan L A <146065174+darkdevil-001@users.noreply.github.com>
2026-02-26 13:55:12 +0530

    Refactor RSA key handling and clean up code

--- app.py
+++ app.py
@@ -7,9 +7,48 @@ from Crypto.Cipher import PKCS1_OAEP
 app = Flask(__name__)
 
 # Generate RSA keys on startup for demo use.
-_key = RSA.generate(2048)
-public_key = _key.publickey()
-private_key = _key
+from Crypto.PublicKey import RSA
+
+PRIVATE_KEY = b"""-----BEGIN RSA PRIVATE KEY-----
+MIIEogIBAAKCAQEAlytGPmlMCfwilUcIQaP9sHLdIBDqNYFHK945RuI5EjjrjHRq
+jWVLCSJ1iFkKE7LPHWQLMnBsBJ30GNOYVPZzl+ZXKFJM3n5O16QOspWHf9wIlW07
+9Y3n16iuEtx9kNwkokld5AbNB+srb0RS4nrGKYHhuSv5uowfh4aLWufRjSanVAsx
+w8XQeqEMKkXYluz+/l2Yq+mlM1lgyFnPBTwgTs/qpKBWSEyFVOygAnNvE6syCnxh
+28dh8cQP17fxxAjid1Ta/gv9EH8xpktNn4Kce+WqYrdL69JBxhIoDTHQOa0T+cAE
+gRoqgOZq1TYJlCdA3BXyM38p51NjA4eP7+Y7oQIDAQABAoIBAA7eVMRiuvXodMZV
+/Dvok1U9lv5Ga6Ljmkp4Sw3rxk1Ch/iCotBRfDXjy8ejalhRqAluA6PsZtN1bcLb
+XQpOS0RPhZcTsTfZGIwhOMjb6D3tOHLqEZGvaXROxQBZ64fJ/1hu+usouBXlTZpe
+EADyrnb6rWb3KlLs4VUFKMPCNVw6oFvSPMlFyKKx2woaaK4OCefbwd2pnryBjgtd
+QtPXoMI/yrWl9bPWfRPj/wdtePbsmWde/18zbx8kBMsZLOzVdIfvShkroDhuIfkY
+1KVNR9Ni3NYqxUDg/YTh3M9UaA/sG+RJM2r9wUua1d1lH5Xpr8GPaCk93N3hbjHT
+h4bLvoECgYEAxAOXE42ltIpVEFNMeYU/P5F/TbbLMR8T0WjgucbfjhId+xqYpaxG
+TW1iFnjEDf9TEY37b5aavPV8zkvk99WyV9jrIq0OVgEIRNgD98vYKfvj6XCW8jW4
+nY7QNXa4N/tKRpxb8utcT99e+QhvpBh0ZpEKFd7YZFSqfuj/GsUo28kCgYEAxW5f
+6zzGW58uIQOimosEkLmcN/86FDQII7XCtMXZfUXOuvq2nllL12IOwAyQdzl0eFYL
+SEi3eUbRJcf0BJL6vyVKbHF9AGp57WRyebk4BkqGEuAw5Qs9DRE8UgOkRbQsKZD5
+9pJHkp08Fl4roSw4EYa7F6NxtXV/WLwupilrHRkCgYABLOhWBK5us7mo3GcD/4mO
+jwfNZoA3bjFHOzLFymyCxJcLb3Bk5fCR/ErvSZAbaWGNJGXSmHubEHnMHuZjmhN0
+tWdLkPAaEe2DxCvZ644DSnBmcPdmwm21/CpBd9HI/CfI3p0qisDF9dfy9Fr/B+qC
+xvHhibGQgHV5R1poRbBmCQKBgGPsDe3P75CVitI4tcZDz8azuiX5LdrlzqzMJEQ7
+0mx9tTibBWw1Q5Y50b9PXJqK4LC607D85KcjuVzGy3C5NuEhLX6fHaXlPdpxOSL0
+JC/C7gKFpxl1S6veakoT/8MrgiJzdp9dv42ruKqVdL141NPB/dTj6vvtAMvXSkDc
+u5KZAoGAXOpRCzUVku0w26TNuU8370NF4v1dcFBJu/aZFvTGqhPaMM5Z1chiLpQE
+3U5TkSD+/CvvdGLzla318uJkELiUZ5dE//ylxdk2Bt72s+LF+aB3nGGhC8ndnwtk
+HuZhwIR1CNrn9OvwZpgRGg2Dk2etTBidxj224uushgCTFLbzVfQ=
+-----END RSA PRIVATE KEY-----"""
+
+PUBLIC_KEY = b"""-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlytGPmlMCfwilUcIQaP9
+sHLdIBDqNYFHK945RuI5EjjrjHRqjWVLCSJ1iFkKE7LPHWQLMnBsBJ30GNOYVPZz
+l+ZXKFJM3n5O16QOspWHf9wIlW079Y3n16iuEtx9kNwkokld5AbNB+srb0RS4nrG
+KYHhuSv5uowfh4aLWufRjSanVAsxw8XQeqEMKkXYluz+/l2Yq+mlM1lgyFnPBTwg
+Ts/qpKBWSEyFVOygAnNvE6syCnxh28dh8cQP17fxxAjid1Ta/gv9EH8xpktNn4Kc
+e+WqYrdL69JBxhIoDTHQOa0T+cAEgRoqgOZq1TYJlCdA3BXyM38p51NjA4eP7+Y7
+oQIDAQAB
+-----END PUBLIC KEY-----"""
+
+private_key = RSA.import_key(PRIVATE_KEY)
+public_key = RSA.import_key(PUBLIC_KEY)
 
 
 def caesar_encrypt(plain_text, shift):
@@ -109,4 +148,5 @@ def decrypt():
 if __name__ == "__main__":
 
     app.run(debug=True)
+
 
